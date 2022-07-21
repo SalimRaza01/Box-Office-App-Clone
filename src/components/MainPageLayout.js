7cafc1f41f2ab088cf1fa1dadbda223942e4cce6
Salim Raza <Salim.creation2017@gmail.com>
2022-07-21 12:56:35 +0530

    Added Final

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navs from './Navs';
 import Title from './Title';
-import logo from '../images/Mylogo.png';
 
 const MainPageLayout = ({ children }) => {
   return (
     <div>
-      <img src={logo} alt="Logo" />
       <Title
         title="Box Office"
         subtitle="Are you looking for a movie or an actor?"
